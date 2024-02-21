ad6c93ef68b0de54c1626676a5fa890017a287b3
archeryfox <89808784+archeryfox@users.noreply.github.com>
2024-02-22 00:58:53 +0300

    d

--- apteka/settings.py
+++ apteka/settings.py
@@ -1,9 +1,14 @@
-
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
+
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:3000']
 
 
 # Quick-start development settings - unsuitable for production
@@ -17,7 +22,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['thunderer77.pythonanywhere.com', '127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -27,12 +31,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'api'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -45,7 +51,7 @@ ROOT_URLCONF = 'apteka.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -60,7 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'apteka.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -70,7 +75,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -90,7 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -101,8 +104,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
--- apteka/urls.py
+++ apteka/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path
 from api.views import *
@@ -6,7 +5,15 @@ from api.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', get_users),
+    path('user/<int:id>/get/', get_user),
+    path('user/<int:id>/', update_user, name='update_user'),
+    path('user/add/', add_user, name='add'),
+    path('user/<int:id>/delete/', delete_user, name='delete_user'),
+    path('meds/<int:pk>/delete/', MedicineDeleteView.as_view(), name='medicine_delete'),
+    path('meds/<int:pk>/update/', MedicineUpdateView.as_view(), name='upd_med'),
+    path('meds/<int:med_id>/buyby/<int:us_id>/', buy, name='buy'),
     path('checks/', get_checks),
     path('recipes/', get_recipes),
-    path('meds/', get_meds),
+    path('recipes/add', add_recipes),
+    path('meds/', get_meds, name="meds-list"),
 ]
